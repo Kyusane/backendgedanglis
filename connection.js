8d65dcc371f671ac0a386d590caa88a06bff342a
Rahmat Rohmani <rahmatrohmani21@gmail.com>
2023-12-21 00:19:17 +0700

    ASDas

--- connection.js
+++ connection.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql')
-// const db = mysql.createConnection({
-//      host :"127.0.0.1", 
-//      user :"root",
-//      password:"TVZ455r11zBlpFRl",
-//      database:"mbkmgedanglis"
-// })
-
 const db = mysql.createConnection({
-     host :"127.0.0.1",
+     host :"127.0.0.1", 
      user :"root",
-     password:"",
+     password:"TVZ455r11zBlpFRl",
      database:"mbkmgedanglis"
 })
+
+// const db = mysql.createConnection({
+//      host :"127.0.0.1",
+//      user :"root",
+//      password:"",
+//      database:"mbkmgedanglis"
+// })
 
 module.exports = db;

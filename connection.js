b4b45fb5b5a735731c75f1fdb1f08c00cf30158d
Rahmat Rohmani <rahmatrohmani21@gmail.com>
2023-12-21 07:44:44 +0700

    change env

--- connection.js
+++ connection.js
@@ -1,16 +1,11 @@
+require("dotenv").config();
 const mysql = require('mysql')
-const db = mysql.createConnection({
-     host :"127.0.0.1", 
-     user :"root",
-     password:"TVZ455r11zBlpFRl",
-     database:"mbkmgedanglis"
-})
 
-// const db = mysql.createConnection({
-//      host :"127.0.0.1",
-//      user :"root",
-//      password:"",
-//      database:"mbkmgedanglis"
-// })
+const db = mysql.createConnection({
+     host :process.env.HOST_DB,
+     user :process.env.USER_DB,
+     password:process.env.PASSWORD_DB,
+     database:process.env.DB_NAME
+})
 
 module.exports = db;
